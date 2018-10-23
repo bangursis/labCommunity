41b6b5e83d8576fee7ad54302d415eee9e5948f1
beingofhabits <beingofhabits@gmail.com>
2018-10-24 00:12:38 +0300

    A few changes are done

--- force-app/main/default/aura/partyContainer/partyContainerController.js
+++ force-app/main/default/aura/partyContainer/partyContainerController.js
@@ -1,5 +1,10 @@
 ({
      switchTab: function(component, event) {
         component.set("v.selectedTab", event.getSource().get("v.value"));
-    }
+    },
+
+    handeTableSelection: function(component, event) {
+           
+    },
+
 })
--- force-app/main/default/aura/partyTakeABooze/partyTakeABoozeController.js
+++ force-app/main/default/aura/partyTakeABooze/partyTakeABoozeController.js
@@ -1,11 +1,11 @@
 ({
     onInit : function(component) {
         let action = component.get("c.checkBoozes");
+        action.setStorable();
         $A.enqueueAction(action);
 
         action.setCallback(this, res => {
             if(res.getState() == "SUCCESS"){
-                console.log(res.getReturnValue());
                 component.set("v.boozes", res.getReturnValue());
             }
         })
