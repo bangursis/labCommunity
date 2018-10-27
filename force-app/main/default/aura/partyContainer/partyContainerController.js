37c872a704c9813aac040e9a70ece736de237509
beingofhabits <beingofhabits@gmail.com>
2018-10-27 23:08:48 +0300

    Dummy Fix

--- force-app/main/default/aura/partyContainer/partyContainerController.js
+++ force-app/main/default/aura/partyContainer/partyContainerController.js
@@ -1,18 +1,13 @@
 ({
      switchTab: function(component, event) {
-        component.set("v.selectedTab", event.getSource().get("v.value"));
-    },
-
-    handeTableSelection: function(component, event) {
-          let tabs = component.find('button') ;
+        let selected = event.getSource().get("v.value");
+        component.set("v.selectedTab", selected);
+        let tabs = component.find('button') ;
 
           tabs.forEach(tab => {
-              let selected = event.getSource().get('v.value');
               let current = tab.get('v.value');
 
-              selected == current ? $A.util.addClass('selectedTab') : $A.util.removeClass('selectedTab');
-              console.log($A.util.hasClass(tab, 'selectedTab') + current);
+              selected == current ? $A.util.addClass(tab, 'selectedTab') : $A.util.removeClass(tab, 'selectedTab');
           });
     },
-
 })
