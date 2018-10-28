83b9ec7acf9614af1d7a9ce68512935a6408cc03
beingofhabits <beingofhabits@gmail.com>
2018-10-28 22:08:01 +0300

    pick a booze is done

--- .sfdx/typings/lwc/apex/PartyController.d.ts
+++ .sfdx/typings/lwc/apex/PartyController.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/PartyController.checkBoozes" {
   export default function checkBoozes(): Promise<any>;
 }
+declare module "@salesforce/apex/PartyController.pickABooze" {
+  export default function pickABooze(param: {bId: any, amount: any}): Promise<any>;
+}
--- force-app/main/default/aura/partyTakeABooze/partyTakeABoozeController.js
+++ force-app/main/default/aura/partyTakeABooze/partyTakeABoozeController.js
@@ -10,14 +10,33 @@
                 component.set("v.boozes", boozes);
                 component.set("v.selectedAmount", boozes[0].Amount__c);
                 
-                console.log(component.get("v.selectedAmount"));
             }
         })
     },
 
     setAmount: function(component, event){
-        let amount = event.getSource().get('v.value');
-        component.set("v.selectedAmount", amount);
-        console.log(component.get("v.selectedAmount"));
+        let boozes = component.get("v.boozes");
+        let index = event.getSource().get('v.value');
+        component.set("v.selectedAmount", boozes[index].Amount__c);
+    },
+
+    take : function(component){
+        let index = component.find("pickABooze").get("v.value");
+        let booze = component.get("v.boozes")[index || 0];
+        let bId = booze.Id;
+        let amount = component.get("v.selectedAmount") - component.find("pickedAmount").get('v.value');
+
+        console.log(`${bId}    ${amount}`);
+
+        let action = component.get("c.pickABooze");
+        action.setParams({
+            bId, amount
+        });
+        $A.enqueueAction(action);
+
+        action.setCallback(this, res => {
+            console.log(res.getState());
+        })
     }
 })
+ 
