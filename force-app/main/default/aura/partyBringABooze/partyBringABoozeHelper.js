a190845c8c371b52be2bb803eadbfaa3e773888a
beingofhabits <beingofhabits@gmail.com>
2018-11-02 00:00:55 +0300

    Bring Helper : firing event

--- force-app/main/default/aura/partyBringABooze/partyBringABoozeHelper.js
+++ force-app/main/default/aura/partyBringABooze/partyBringABoozeHelper.js
@@ -1,5 +1,11 @@
 ({
-    helperMethod : function() {
+    bringNotification : function(type, amount) {
+        let evt = $A.get("e.c:bringBoozes");
 
+        evt.setParams({
+            bringedAmount : new Map([[type, amount]])
+        });
+
+        evt.fire();
     }
 })
